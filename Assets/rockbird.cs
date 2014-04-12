2f5d53d36d51d513e1093ce468287b13c6ff8c61
Waisze Lam <lam.waisze@gmail.com>
2014-04-12 19:29:13 -0400

    initial commit

---
+++ Assets/rockbird.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rockbird : MonoBehaviour {
+
+
+	public int counter;
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((counter % 60) == 0) {
+			//gameObject.AddForce = new Vector3 (0, -10f, 0);
+			counter = 0;
+		}
+		counter++;
+	}
+}
