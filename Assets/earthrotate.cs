dfc69a916c4a24d601eeaed722da23818debaf05
Waisze Lam <lam.waisze@gmail.com>
2014-04-12 20:39:39 -0400

    added earth rotate

---
+++ Assets/earthrotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class earthrotate : MonoBehaviour {
+	public float rotate_speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(0, rotate_speed, 0);
+	}
+}
